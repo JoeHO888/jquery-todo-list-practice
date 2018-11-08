87ce2534ea87a408e7aabd5046c2fc520b2d8f71
JoeHo <joe.hochotai@gmail.com>
2018-11-08 17:05:26 +0800

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,5 @@
+
+var boxes;
 $(document)
     .ready(function () {
 
@@ -22,4 +24,58 @@ $(document)
         }
 
         // code to be implemented
-    });
+		
+		// Add Item
+		$boxes = $(":checkbox");
+		$("#button").click(function(){
+			var uuid = generateUUID();
+			var item = $("input[name='ListItem']").val();
+			$("ol").append("<li id="+uuid+" class=''>"+"<input name='done-todo' type='checkbox' class='done-todo' onclick='handleClick(this)'>"+item+"</li>");
+			$boxes = $(":checkbox");
+			
+		});
+		
+		
+});
+
+function handleClick(cb) {
+  var parentId = cb.parentNode.id;
+  if (cb.checked){
+	  $("#"+parentId).addClass("checked");
+
+	  
+  }else{
+	  $("#"+parentId).removeClass("checked");
+  }
+}
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+	
+showTime();
